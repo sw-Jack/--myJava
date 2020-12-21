7973fb471200d61f9b93bf3a846a51af46531019
sw-Jack <jacksuwon@gmail.com>
2020-12-21 21:56:51 +0900

    자바 기본

---
+++ day01/CommentEx.java
@@ -0,0 +1,23 @@
+/** 
+	javadoc 주석 설명
+*/
+/**
+	<font color="navy" size="5">
+	"go for it!!"
+	</font>
+*/
+
+public class CommentEx {
+	/*
+		자바에서 프로그램의 시작점을 알려주는 <font color="red"> main() </font> 메서드 입니다. <br>
+		public static void main(String [] 변수명) 의 형태를 갖습니다.
+		위에서 한글자라도 틀리면 에러입니다. 오직 변수명만 사용자 정의해서 사용할 수 있습니다.
+	*/
+	public static void main(String[] args) {
+		// 화면에 출력 - 한 줄 주석 처리
+		System.out.println("go for it!!");	// go for it!! 이 출력된다.
+		/*
+			이 사이에 주석의 내용을 기재.
+		*/
+	}
+}
