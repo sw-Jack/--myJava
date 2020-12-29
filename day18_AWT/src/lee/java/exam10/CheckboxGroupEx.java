29efd612659bdaf2960f03db97eccfc317f6c04a
sw-Jack <jacksuwon@gmail.com>
2020-12-29 22:11:09 +0900

    자바 Event - CheckboxGroup

---
+++ day18_AWT/src/lee/java/exam10/CheckboxGroupEx.java
@@ -0,0 +1,29 @@
+package lee.java.exam10;
+
+import java.awt.*;
+
+public class CheckboxGroupEx extends Frame {
+
+	GridLayout gl = new GridLayout(4,1);
+	Label lab = new Label("성별은?");
+	CheckboxGroup cg = new CheckboxGroup();
+	Checkbox man = new Checkbox("남성", cg, true);
+	Checkbox woman = new Checkbox("여성", cg, false);
+	Button bt = new Button("확인");
+	
+	public CheckboxGroupEx() {
+		super("CheckboxGroup");
+		setLayout(gl);
+		add(lab);
+		add(man);
+		add(woman);
+		add(bt);
+		pack();
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		CheckboxGroupEx cg = new CheckboxGroupEx();
+	}
+
+}
