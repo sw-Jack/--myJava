af53c2fb8b5732196e34956d27a5f0113eec31e5
sw-Jack <jacksuwon@gmail.com>
2020-12-29 22:12:25 +0900

    자바 Event - FileDialog

---
+++ day18_AWT/src/lee/java/exam13/FileDialogEx.java
@@ -0,0 +1,25 @@
+package lee.java.exam13;
+import java.awt.FileDialog;
+import java.awt.Frame;
+
+// File Dialog 예제
+public class FileDialogEx extends Frame {
+	// [멤버]
+	FileDialog fileDialog = new FileDialog(this, "저장하기 테스트", FileDialog.SAVE);
+	
+	// [생성자]
+	public FileDialogEx() {
+		super("FileDialogEx");
+		setSize(200,300);
+		setVisible(true);
+		fileDialog.setVisible(true);
+		
+		System.out.println(fileDialog.getFile());
+		System.out.println(fileDialog.getDirectory());
+	}
+	
+	// [메서드]
+	public static void main(String[] args) {
+		FileDialogEx fileDialogEx = new FileDialogEx();
+	}
+}
