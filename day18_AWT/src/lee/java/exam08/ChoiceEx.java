3773023c059ab131b2b3578cc090f342b807897a
sw-Jack <jacksuwon@gmail.com>
2020-12-29 22:10:40 +0900

    자바 Event - Choice

---
+++ day18_AWT/src/lee/java/exam08/ChoiceEx.java
@@ -0,0 +1,27 @@
+package lee.java.exam08;
+
+import java.awt.*;
+
+public class ChoiceEx extends Frame {
+
+	private Label lab = new Label("»ýÀÏ");
+	private Choice ch = new Choice();
+	private Button bt = new Button();
+	
+	public ChoiceEx() {
+		super("Choice");
+		setLayout(new GridLayout(3,1));
+		add(lab);
+		for(int i=1; i<=12; i++) {
+			ch.add(String.valueOf(i) + "¿ù");
+		}
+		add(ch);
+		add(bt);
+		pack();
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		ChoiceEx choice = new ChoiceEx();
+	}
+
+}
