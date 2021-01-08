ec41d04645e7ce2bbc4610f4620d637b7267aca0
sw-Jack <jacksuwon@gmail.com>
2021-01-08 21:39:34 +0900

    Applet - FontMetrics

---
+++ Applet/FontMetricsEx.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.applet.*;
+
+public class FontMetricsEx extends Applet {
+	Font f;
+	FontMetrics fm;
+	int x,y;
+	String msg;
+	Dimension size;
+	public void init() {
+		setBackground(Color.yellow);
+		msg = "FontMetrics";
+		f = new Font("impact", Font.BOLD, 30);
+		fm = getFontMetrics(f);
+		size = getSize();
+		x = (size.width/2) - (fm.stringWidth(msg)/2);
+		y = (size.height/2) - (fm.getDescent()/2);
+	}
+
+	public void paint(Graphics g) {
+		g.setFont(f);
+		g.drawString(msg, x, y);
+	}
+}
