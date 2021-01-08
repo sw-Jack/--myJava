8595bc8971ab18c564ee8f8a3958e5d9afe3bf8c
sw-Jack <jacksuwon@gmail.com>
2021-01-08 21:31:41 +0900

    Applet - AudioClip

---
+++ Applet/AudioEx.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.applet.*;
+
+public class AudioEx extends Applet {
+	private AudioClip audio;
+	private Label lab;
+
+	public void init() {
+		lab = new Label("Can you hear the sounds?", Label.CENTER);
+		lab.setFont(new Font("±¼¸²Ã¼", Font.BOLD, 25));
+		setLayout(new BorderLayout());
+		add("North", lab);
+		try {
+			audio = getAudioClip(getCodeBase(), "image/spacemusic.au");
+		} catch(Exception e) {}
+	}
+
+	public void start() {
+		try {
+			audio.loop();
+		} catch(Exception e) {}
+	}
+}
