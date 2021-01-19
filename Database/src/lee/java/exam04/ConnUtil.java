f0a1f1717cefb1a6763508eb2282df9ea90ae91d
sw-Jack <jacksuwon@gmail.com>
2021-01-19 21:30:36 +0900

    Database - Connection Util class

---
+++ Database/src/lee/java/exam04/ConnUtil.java
@@ -0,0 +1,21 @@
+package lee.java.exam04;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnUtil {
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getCOnConnection() throws SQLException {
+		return DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521:xe",
+				"jobclass", "jobclass");
+	}
+}
